d8432cfea5237cb7012cf0e9331d09a1f364d96e
Azat Akhunov <azat.akhunov@protonmail.com>
2020-06-06 14:47:08 +0300

    [NF] send video were added

---
+++ src/main/java/com/botmasterzzz/bot/api/impl/methods/send/SendVideo.java
@@ -0,0 +1,241 @@
+package com.botmasterzzz.bot.api.impl.methods.send;
+
+import com.botmasterzzz.bot.api.impl.methods.PartialBotApiMethod;
+import com.botmasterzzz.bot.api.impl.objects.InputFile;
+import com.botmasterzzz.bot.api.impl.objects.Message;
+import com.botmasterzzz.bot.api.impl.objects.replykeyboard.ApiResponse;
+import com.botmasterzzz.bot.api.impl.objects.replykeyboard.ReplyKeyboard;
+import com.botmasterzzz.bot.exceptions.TelegramApiRequestException;
+import com.botmasterzzz.bot.exceptions.TelegramApiValidationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+public class SendVideo extends PartialBotApiMethod<Message> {
+    public static final String PATH = "sendvideo";
+
+    public static final String CHATID_FIELD = "chat_id";
+    public static final String VIDEO_FIELD = "video";
+    public static final String DURATION_FIELD = "duration";
+    public static final String CAPTION_FIELD = "caption";
+    public static final String WIDTH_FIELD = "width";
+    public static final String HEIGHT_FIELD = "height";
+    public static final String DISABLENOTIFICATION_FIELD = "disable_notification";
+    public static final String SUPPORTSSTREAMING_FIELD = "supports_streaming";
+    public static final String REPLYTOMESSAGEID_FIELD = "reply_to_message_id";
+    public static final String REPLYMARKUP_FIELD = "reply_markup";
+    public static final String PARSEMODE_FIELD = "parse_mode";
+    public static final String THUMB_FIELD = "thumb";
+
+    private String chatId;
+    private InputFile video;
+    private Integer duration;
+    private String caption;
+    private Integer width;
+    private Integer height;
+    private Boolean supportsStreaming;
+    private Boolean disableNotification;
+    private Integer replyToMessageId;
+    private ReplyKeyboard replyMarkup;
+    private String parseMode;
+
+    private InputFile thumb;
+
+    public SendVideo() {
+        super();
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public SendVideo setChatId(String chatId) {
+        this.chatId = chatId;
+        return this;
+    }
+
+    public InputFile getVideo() {
+        return video;
+    }
+
+    public SendVideo setVideo(String video) {
+        this.video = new InputFile(video);
+        return this;
+    }
+
+    public SendVideo setChatId(Long chatId) {
+        Objects.requireNonNull(chatId);
+        this.chatId = chatId.toString();
+        return this;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public SendVideo setDuration(Integer duration) {
+        this.duration = duration;
+        return this;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public SendVideo setCaption(String caption) {
+        this.caption = caption;
+        return this;
+    }
+
+    public Integer getReplyToMessageId() {
+        return replyToMessageId;
+    }
+
+    public SendVideo setReplyToMessageId(Integer replyToMessageId) {
+        this.replyToMessageId = replyToMessageId;
+        return this;
+    }
+
+    public ReplyKeyboard getReplyMarkup() {
+        return replyMarkup;
+    }
+
+    public SendVideo setReplyMarkup(ReplyKeyboard replyMarkup) {
+        this.replyMarkup = replyMarkup;
+        return this;
+    }
+
+    public Boolean getDisableNotification() {
+        return disableNotification;
+    }
+
+    public SendVideo enableNotification() {
+        this.disableNotification = false;
+        return this;
+    }
+
+    public SendVideo disableNotification() {
+        this.disableNotification = true;
+        return this;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public SendVideo setWidth(Integer width) {
+        this.width = width;
+        return this;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public SendVideo setHeight(Integer height) {
+        this.height = height;
+        return this;
+    }
+
+    public SendVideo setVideo(InputFile video) {
+        Objects.requireNonNull(video, "video cannot be null!");
+        this.video = video;
+        return this;
+    }
+
+    public SendVideo setVideo(File file) {
+        Objects.requireNonNull(file, "file cannot be null!");
+        this.video = new InputFile(file, file.getName());
+        return this;
+    }
+
+    public SendVideo setVideo(String videoName, InputStream inputStream) {
+        Objects.requireNonNull(videoName, "videoName cannot be null!");
+        Objects.requireNonNull(inputStream, "inputStream cannot be null!");
+        this.video = new InputFile(inputStream, videoName);
+        return this;
+    }
+
+    public Boolean getSupportsStreaming() {
+        return supportsStreaming;
+    }
+
+    public SendVideo setSupportsStreaming(Boolean supportsStreaming) {
+        this.supportsStreaming = supportsStreaming;
+        return this;
+    }
+
+    public String getParseMode() {
+        return parseMode;
+    }
+
+    public SendVideo setParseMode(String parseMode) {
+        this.parseMode = parseMode;
+        return this;
+    }
+
+    public InputFile getThumb() {
+        return thumb;
+    }
+
+    public SendVideo setThumb(InputFile thumb) {
+        this.thumb = thumb;
+        return this;
+    }
+
+    @Override
+    public Message deserializeResponse(String answer) throws TelegramApiRequestException {
+        try {
+            ApiResponse<Message> result = OBJECT_MAPPER.readValue(answer,
+                    new TypeReference<ApiResponse<Message>>(){});
+            if (result.getOk()) {
+                return result.getResult();
+            } else {
+                throw new TelegramApiRequestException("Error sending video", result);
+            }
+        } catch (IOException e) {
+            throw new TelegramApiRequestException("Unable to deserialize response", e);
+        }
+    }
+
+    @Override
+    public void validate() throws TelegramApiValidationException {
+        if (chatId == null) {
+            throw new TelegramApiValidationException("ChatId parameter can't be empty", this);
+        }
+
+        if (video == null) {
+            throw new TelegramApiValidationException("Video parameter can't be empty", this);
+        }
+
+        video.validate();
+
+        if (thumb != null) {
+            thumb.validate();
+        }
+        if (replyMarkup != null) {
+            replyMarkup.validate();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SendVideo{" +
+                "chatId='" + chatId + '\'' +
+                ", video=" + video +
+                ", duration=" + duration +
+                ", caption='" + caption + '\'' +
+                ", width=" + width +
+                ", height=" + height +
+                ", supportsStreaming=" + supportsStreaming +
+                ", disableNotification=" + disableNotification +
+                ", replyToMessageId=" + replyToMessageId +
+                ", replyMarkup=" + replyMarkup +
+                ", parseMode='" + parseMode + '\'' +
+                ", thumb=" + thumb +
+                '}';
+    }
+}
---
+++ src/main/java/com/botmasterzzz/bot/api/impl/methods/send/SendVideoNote.java
@@ -0,0 +1,227 @@
+package com.botmasterzzz.bot.api.impl.methods.send;
+
+import com.botmasterzzz.bot.api.impl.methods.PartialBotApiMethod;
+import com.botmasterzzz.bot.api.impl.objects.InputFile;
+import com.botmasterzzz.bot.api.impl.objects.Message;
+import com.botmasterzzz.bot.api.impl.objects.replykeyboard.ApiResponse;
+import com.botmasterzzz.bot.api.impl.objects.replykeyboard.ReplyKeyboard;
+import com.botmasterzzz.bot.exceptions.TelegramApiRequestException;
+import com.botmasterzzz.bot.exceptions.TelegramApiValidationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+@SuppressWarnings({"UnusedReturnValue", "WeakerAccess"})
+public class SendVideoNote extends PartialBotApiMethod<Message> {
+    public static final String PATH = "sendvideonote";
+
+    public static final String CHATID_FIELD = "chat_id";
+    public static final String VIDEONOTE_FIELD = "video_note";
+    public static final String DURATION_FIELD = "duration";
+    public static final String LENGTH_FIELD = "length";
+    public static final String DISABLENOTIFICATION_FIELD = "disable_notification";
+    public static final String REPLYTOMESSAGEID_FIELD = "reply_to_message_id";
+    public static final String REPLYMARKUP_FIELD = "reply_markup";
+    public static final String THUMB_FIELD = "thumb";
+
+    private String chatId;
+    private InputFile videoNote;
+    private Integer duration;
+    private Integer length;
+    private Boolean disableNotification;
+    private Integer replyToMessageId;
+    private ReplyKeyboard replyMarkup;
+
+    private InputFile thumb;
+
+    public SendVideoNote() {
+        super();
+    }
+
+
+    public SendVideoNote(String chatId, String videoNote) {
+        this.chatId = checkNotNull(chatId);
+        this.setVideoNote(checkNotNull(videoNote));
+    }
+
+    public SendVideoNote(Long chatId, String videoNote) {
+
+        this.chatId = checkNotNull(chatId).toString();
+        this.setVideoNote(checkNotNull(videoNote));
+    }
+
+    public SendVideoNote(String chatId, File videoNote) {
+        this.chatId = checkNotNull(chatId);
+        this.setVideoNote(videoNote);
+    }
+
+    public SendVideoNote(Integer chatId, File videoNote) {
+        this.chatId = checkNotNull(chatId).toString();
+        this.setVideoNote(videoNote);
+    }
+
+    public SendVideoNote(String chatId, String videoNoteName, InputStream videoNote) {
+        this.chatId = checkNotNull(chatId);
+        this.setVideoNote(videoNoteName, videoNote);
+    }
+
+    public SendVideoNote(Integer chatId, String videoNoteName, InputStream videoNote) {
+        this.setVideoNote(videoNoteName, videoNote);
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public SendVideoNote setChatId(String chatId) {
+        this.chatId = chatId;
+        return this;
+    }
+
+    public InputFile getVideoNote() {
+        return videoNote;
+    }
+
+    public SendVideoNote setVideoNote(String videoNote) {
+        this.videoNote = new InputFile(videoNote);
+        return this;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public SendVideoNote setLength(Integer length) {
+        this.length = length;
+        return this;
+    }
+
+    public SendVideoNote setChatId(Long chatId) {
+        Objects.requireNonNull(chatId);
+        this.chatId = chatId.toString();
+        return this;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public SendVideoNote setDuration(Integer duration) {
+        this.duration = duration;
+        return this;
+    }
+
+    public Integer getReplyToMessageId() {
+        return replyToMessageId;
+    }
+
+    public SendVideoNote setReplyToMessageId(Integer replyToMessageId) {
+        this.replyToMessageId = replyToMessageId;
+        return this;
+    }
+
+    public ReplyKeyboard getReplyMarkup() {
+        return replyMarkup;
+    }
+
+    public SendVideoNote setReplyMarkup(ReplyKeyboard replyMarkup) {
+        this.replyMarkup = replyMarkup;
+        return this;
+    }
+
+    public Boolean getDisableNotification() {
+        return disableNotification;
+    }
+
+    public SendVideoNote enableNotification() {
+        this.disableNotification = false;
+        return this;
+    }
+
+    public SendVideoNote disableNotification() {
+        this.disableNotification = true;
+        return this;
+    }
+
+    public SendVideoNote setVideoNote(InputFile videoNote) {
+        Objects.requireNonNull(videoNote, "videoNote cannot be null!");
+        this.videoNote = videoNote;
+        return this;
+    }
+
+    public SendVideoNote setVideoNote(File file) {
+        Objects.requireNonNull(file, "file cannot be null!");
+        this.videoNote = new InputFile(file, file.getName());
+        return this;
+    }
+
+    public SendVideoNote setVideoNote(String videoName, InputStream inputStream) {
+        Objects.requireNonNull(videoName, "videoName cannot be null!");
+        Objects.requireNonNull(inputStream, "inputStream cannot be null!");
+        this.videoNote = new InputFile(inputStream, videoName);
+        return this;
+    }
+
+    public InputFile getThumb() {
+        return thumb;
+    }
+
+    public SendVideoNote setThumb(InputFile thumb) {
+        this.thumb = thumb;
+        return this;
+    }
+
+    @Override
+    public Message deserializeResponse(String answer) throws TelegramApiRequestException {
+        try {
+            ApiResponse<Message> result = OBJECT_MAPPER.readValue(answer,
+                    new TypeReference<ApiResponse<Message>>(){});
+            if (result.getOk()) {
+                return result.getResult();
+            } else {
+                throw new TelegramApiRequestException("Error sending video note", result);
+            }
+        } catch (IOException e) {
+            throw new TelegramApiRequestException("Unable to deserialize response", e);
+        }
+    }
+
+    @Override
+    public void validate() throws TelegramApiValidationException {
+        if (chatId == null) {
+            throw new TelegramApiValidationException("ChatId parameter can't be empty", this);
+        }
+
+        if (videoNote == null) {
+            throw new TelegramApiValidationException("VideoNote parameter can't be empty", this);
+        }
+
+        videoNote.validate();
+
+        if (thumb != null) {
+            thumb.validate();
+        }
+        if (replyMarkup != null) {
+            replyMarkup.validate();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SendVideoNote{" +
+                "chatId='" + chatId + '\'' +
+                ", videoNote=" + videoNote +
+                ", duration=" + duration +
+                ", length=" + length +
+                ", disableNotification=" + disableNotification +
+                ", replyToMessageId=" + replyToMessageId +
+                ", replyMarkup=" + replyMarkup +
+                ", thumb=" + thumb +
+                '}';
+    }
+}
